77b44815e0d105d25638fdf4913580174753cb7d
chaluvadis <surendra.chaluvadi@gmail.com>
2018-04-22 22:38:30 -0500

    list lambda updated

--- speakers/listSpeakers.js
+++ speakers/listSpeakers.js
@@ -1,24 +1,32 @@
 "use strict";
 module.exports.list = (event, context, callback) => {
-    var faker = require("faker");
-    var _ = require("lodash");
-    var assignments = _.times(100, n => {
-      return {
-        id: faker.random.number(1000, 2000),
-        clientname: faker.name.jobTitle(),
-        assignmentguid: faker.random.uuid(),
-        questassignmentid:
-          faker.random.number(100000, 999999).toString() +
-          "-" +
-          faker.random.number(1000, 9999).toString(),
-        isActive: true,
-        createdDate: faker.date.recent()
-      };
-    });
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(assignments)
+  var faker = require("faker");
+  var _ = require("lodash");
+  var speakers = _.times(100, n => {
+    var firstName = faker.name.firstName(),
+      lastName = faker.name.lastName(),
+      suffix = faker.name.suffix();
+
+    return {
+      id: faker.random.number(1000, 2000),
+      firstName: firstName,
+      lastName: lastName,
+      suffix: suffix,
+      fullName: suffix + ". " + firstName + " " + lastName,
+      email: lastName + "." + firstName + "@speakers.com",
+      expertsIn: [
+        faker.random.arrayElement(["JavaScript", "TypeScript", "CoffeeScript"]),
+        faker.random.arrayElement(["Web Application", "Mobile Applications", "Single Page Application"]),
+        faker.random.arrayElement(["c#", "Asp.Net", "Asp.Net Core"]),
+      ],
+      city: faker.address.city(),
+      state: faker.address.state(),
+      profile: faker.image.imageUrl()
     };
-    callback(null, response);
+  });
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify(speakers)
   };
-  
+  callback(null, response);
+};
